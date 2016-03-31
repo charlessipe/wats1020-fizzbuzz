562d900a5f00c367e0a9f876b56107a7fb5edf07
csipe84 <csipe84@gmail.com>
2016-03-31 11:28:08 -0400

    Add FizzBuzz for loop

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,24 @@
 // Place your FizzBuzz code here. 
+
+function fizzBuzz(){
+  for(i=1; i<21; i++){
+    if(i%3===0 && i%5===0){  // Check if the number is divisible by 3 and 5
+      console.log(i+" FizzBuzz");
+      document.write(i+" FizzBuzz" + "<br>");
+    }
+    else if(i%3===0){  // Check if the number is divisible by 3
+      console.log(i+" Fizz");
+      document.write(i+" Fizz" + "<br>");
+    }   
+    else if(i%5===0){  // Check if the number is divisible by 5
+      console.log(i+" Buzz");
+      document.write(i+" Buzz" + "<br>");
+    }
+    else {
+      console.log(i);
+      document.write(i + "<br>");
+    }
+  }
+}
+
+fizzBuzz();
